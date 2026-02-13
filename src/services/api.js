41d96b3b3e009b1a7d8a04f8aa31ebb283b8bd50
sarojajeet <sarojajeet660@gmail.com>
2026-02-14 00:03:01 +0530

    check for editor

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://lionfish-app-pk8s6.ondigitalocean.app",
+  // baseURL: "http://localhost:5000/api",
+  baseURL: "https://lionfish-app-pk8s6.ondigitalocean.app/api",
 });
 
 API.interceptors.request.use((req) => {
